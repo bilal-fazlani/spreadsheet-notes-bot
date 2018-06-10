7544ba59cc5aa74dc8b54b79cb44e6db4180bb7d
Bilal Fazlani <bilal.m.fazlani@gmail.com>
2018-06-10 11:46:17 +0530

    parameterised deploy script

--- SpreadsheetTextCapture/build-push-release.sh
+++ SpreadsheetTextCapture/build-push-release.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash -xe
 
+environment=${1:-test}
+
 rm -rf ./publish
 dotnet publish -o ./publish -c release
 rm -rf ./publish/personal
-heroku container:push -v web --app=ops-assistant-test
-heroku container:release web --app=ops-assistant-test
+heroku container:push -v web --app=ops-assistant-${environment}
+heroku container:release web --app=ops-assistant-${environment}
 rm -rf ./publish
 
