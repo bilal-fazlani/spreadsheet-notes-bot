65c4e2ed151c7f501d256a9d9889189f33b55521
Bilal Fazlani <bilal.m.fazlani@gmail.com>
2018-06-10 10:23:30 +0530

    fixed dockerfile and build scripts

--- SpreadsheetTextCapture/build-push-release.sh
+++ SpreadsheetTextCapture/build-push-release.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash -xe
 
-rm -rf ../publish
+rm -rf ./publish
 dotnet build -c release
-dotnet publish -o ../publish -c release
-rm -rf ../publish/personal
+dotnet publish -o ./publish -c release
+rm -rf ./publish/personal
 docker build --no-cache -t opsbot .
 heroku container:push -v web --app=ops-assistant-test
 heroku container:release web --app=ops-assistant-test
+rm -rf ./publish
 
